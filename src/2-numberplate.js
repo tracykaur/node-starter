3c0f0ad8aa2fe27a2b6f4f4becded856d78050a5
tracykaur <tracykaur@yahoo.com>
2017-11-20 16:34:02 +1000

    2numberplate

--- src/2-numberplate.js
+++ src/2-numberplate.js
@@ -1,15 +1,37 @@
 function formatVanityPlate(string) {
   let words = string.split(' ')
   words = words.map(word => word.toUpperCase())
-  return words.join('')
+  let chars = words.join('').split('')
+    if(chars.length > 12){
+  	for (var i = chars.length - 13; i >= 0; i--) {
+   		chars.pop()
+  	}
+   }
+   return chars.join('')
+
 }
 
+function replaceFor(string) {
+  return string.replace('for', '4')
+}
+
+function replaceTo(string) {
+  return string.replace('to', '2')
+}
+
+
+
 const one = 'I like money'
-console.log(`Number plate: ${ formatVanityPlate(one) }`)
+console.log(`Number plate:  ${ formatVanityPlate(replaceFor(one)) }`)
 
 const two = 'Cats for life'
-console.log(`Number plate: ${ formatVanityPlate(two) }`)
+console.log(`Number plate: ${ formatVanityPlate(replaceFor(two)) }`)
 
+const three = 'Go to skool'
+console.log(`Number plate: ${ formatVanityPlate(replaceTo(three)) }`)
+
+const four = 'Life is cool bananas'
+console.log(`Number plate: ${ formatVanityPlate((four)) }`)
 
 /*
 
